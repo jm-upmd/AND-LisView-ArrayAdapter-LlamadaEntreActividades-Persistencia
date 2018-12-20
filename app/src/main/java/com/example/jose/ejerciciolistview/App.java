cde9c6b7b4407757abb9ff9141b650396fbf3ad5
jmdomher <jmdominguez@aytoalcobendas.org>
2018-12-20 01:42:12 +0100

    Versión de la app con persistencia de datos de los Quesos.

---
+++ app/src/main/java/com/example/jose/ejerciciolistview/App.java
@@ -0,0 +1,18 @@
+package com.example.jose.ejerciciolistview;
+
+import android.app.Application;
+import android.content.Context;
+
+public class App extends Application {
+    private static Context appContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        appContext = this;
+    }
+
+    public static Context getAppContext() {
+        return appContext;
+    }
+}
